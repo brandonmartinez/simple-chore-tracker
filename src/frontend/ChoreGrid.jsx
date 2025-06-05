d3567c60959989756850cf778b2588b938db5ef2
Brandon Martinez <me@brandonmartinez.com>
2025-06-05 17:06:57 +0000

    added points

--- src/frontend/ChoreGrid.jsx
+++ src/frontend/ChoreGrid.jsx
@@ -12,17 +12,20 @@ function ChoreGrid({
 	return (
 		<div
 			className="grid gap-4"
-			style={{ gridTemplateColumns: `repeat(${people.length + 1}, 1fr)` }}
+			style={{
+				gridTemplateColumns: `repeat(${people.length + 1}, 1fr) 0.25fr`,
+			}}
 		>
 			<div></div>
 			{people.map((person) => (
 				<div
 					key={person.id}
-					className="font-bold text-center bg-gray-50 p-2 rounded"
+					className="font-bold text-center bg-gray-50 text-gray-700 p-2 rounded"
 				>
 					{person.name}
 				</div>
 			))}
+			<div></div>
 			{sortedCategories.map((category) => (
 				<React.Fragment key={category}>
 					<h3 className="col-span-full text-lg font-semibold text-white bg-gray-800 p-2 rounded">
@@ -32,7 +35,7 @@ function ChoreGrid({
 						.sort((a, b) => a.title.localeCompare(b.title))
 						.map((chore) => (
 							<React.Fragment key={chore.id}>
-								<div className="font-bold text-gray-700 bg-gray-50 p-2 rounded">
+								<div className="font-bold text-gray-700 bg-gray-50 p-2 rounded flex items-center">
 									{chore.title}
 								</div>
 								{people.map((person) => (
@@ -45,6 +48,9 @@ function ChoreGrid({
 										onRemoveAssignment={onRemoveAssignment}
 									/>
 								))}
+								<div className="flex items-center justify-center text-sm font-bold text-gray-300 bg-gray-700 p-1 rounded">
+									+{chore.points}
+								</div>
 							</React.Fragment>
 						))}
 				</React.Fragment>
